86595c803dcc6c207087a19fc7e74d550b5f94a1
Nduati Kuria <012nkuria@gmail.com>
2026-04-16 14:42:39 +0300

    WIP

---
+++ test/support/test_game.ex
@@ -0,0 +1,79 @@
+defmodule AshGleam.TestGame do
+  use Ash.Resource,
+    otp_app: :ash_gleam,
+    domain: AshGleam.TestDomain,
+    data_layer: Ash.DataLayer.Ets,
+    extensions: [AshGleam.Resource, AshGleam.Actions]
+
+  ets do
+    private? false
+    table :ash_gleam_test_games
+  end
+
+  gleam do
+    type_name "Game"
+    module_name "game_item"
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string do
+      allow_nil? false
+      public? true
+    end
+
+    attribute :current_player, :atom do
+      allow_nil? false
+      public? true
+      constraints one_of: [:x, :o]
+      default :x
+    end
+
+    attribute :status, :atom do
+      allow_nil? false
+      public? true
+      constraints one_of: [:in_progress, :finished]
+      default :in_progress
+    end
+
+    attribute :winner, :atom do
+      allow_nil? true
+      public? true
+      constraints one_of: [:x, :o, :draw]
+    end
+  end
+
+  actions do
+    defaults [:read]
+
+    create :create do
+      accept [:name, :current_player, :status, :winner]
+    end
+
+    update :update do
+      accept [:name, :current_player, :status, :winner]
+      require_atomic? false
+    end
+
+    destroy :destroy
+
+    read :get do
+      get_by [:id]
+    end
+  end
+
+  gleam_actions do
+    action :switch_player, __MODULE__ do
+      argument :game, __MODULE__, allow_nil?: false
+
+      run &:test_gleam.switch_player/1
+    end
+
+    action :finish_as_draw, __MODULE__ do
+      argument :game, __MODULE__, allow_nil?: false
+
+      run &:test_gleam.finish_as_draw/1
+    end
+  end
+end
