81a18fc931ff08c05bc370a9658489a58f335a6c
Nduati Kuria <012nkuria@gmail.com>
2026-04-16 13:00:04 +0300

    Prepare example dir

--- example/mix.exs
+++ example/mix.exs
@@ -66,7 +66,9 @@ defmodule Example.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      # 
+      {:ash_gleam, path: ".."},
     ]
   end
 
