cf5c0a1aa93b789f8d3c1497c2037225ec470ede
Nduati Kuria <012nkuria@gmail.com>
2026-04-17 18:09:27 +0300

    Update games.ex

--- example/lib/example/games/games.ex
+++ example/lib/example/games/games.ex
@@ -1,14 +1,7 @@
 defmodule Example.Games do
   use Ash.Domain,
     otp_app: :example,
-    extensions: [AshGleam.FFI]
-
-  gleam_ffi do
-    resource Example.Games.TicTacToe do
-      # action :create_project, :create
-      # action :get_project, :get
-    end
-  end
+    extensions: [AshGleam.Domain]
 
   resources do
     resource Example.Games.TicTacToe do
