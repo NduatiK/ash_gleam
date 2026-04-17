a04af837fab5a6885acbfbb94a3aa7d1132841fe
Nduati Kuria <012nkuria@gmail.com>
2026-04-18 00:01:08 +0300

    Fix bug

--- lib/ash_gleam/codegen/renderer.ex
+++ lib/ash_gleam/codegen/renderer.ex
@@ -130,7 +130,7 @@ defmodule AshGleam.Codegen.Renderer do
 
         payload =
           Enum.map_join(variant.fields, ", ", fn field ->
-            field.name <> ": "<> gleam_type_for_type(field.type, field.constraints, field.allow_nil?, field.name)
+            Atom.to_string(field.name) <> ": "<> gleam_type_for_type(field.type, field.constraints, field.allow_nil?, field.name)
           end)
 
         case variant.fields do
